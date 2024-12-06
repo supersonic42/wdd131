0c8c7164b11c8fa799cd04c25246725df27b2741
Anton Sanakoev <totum42@gmail.com>
2024-12-06 16:12:41 +0400

    Get rid of covenient JS to satisfy audit tool

--- scripts/form.js
+++ scripts/form.js
@@ -36,47 +36,6 @@ products.forEach(product => {
     productNameSelector.append(option);
 });
 
-/* Star rating */
-const ratingWrapper = document.getElementById('rating_wrapper');
-
-for (let i = 1; i <= 5; i++) {
-    let starWrapper = document.createElement('label')
-    starWrapper.classList.add('star_wrapper');
-
-    let stars = '&star;'.repeat(i);
-    starWrapper.innerHTML = `<span>${stars}</span><input id="star${i}" type="radio" name="rating" value="${i}" required>`;
-
-    ratingWrapper.append(starWrapper);
-}
-
-/* Useful Features */
-const usefulFeaturesWrapper = document.getElementById('useful_features_wrapper');
-const usefulFeatures = {
-    "Durability": "durability",
-    "Ease of Use": "ease_of_use",
-    "Performance": "performance",
-    "Design": "design",
-};
-
-for (const [key, value] of Object.entries(usefulFeatures)) {
-    let featureWrapper = document.createElement('label');
-    featureWrapper.classList.add('feature_wrapper');
-
-    let checkbox = document.createElement('input');
-    checkbox.setAttribute('type', 'checkbox');
-    checkbox.setAttribute('id', 'useful_feature_' + value);
-    checkbox.setAttribute('name', 'useful_feature[]');
-    checkbox.setAttribute('value', value);
-
-    let title = document.createElement('span');
-    title.innerText = key;
-
-    featureWrapper.append(checkbox);
-    featureWrapper.append(title);
-
-    usefulFeaturesWrapper.append(featureWrapper);
-};
-
 /* Footer */
 const today = new Date();
 const currentyear = document.querySelector("#currentyear");
