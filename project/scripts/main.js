e19738b139777fba8204d6c73cbb2ae883e8c77a
Anton Sanakoev <totum42@gmail.com>
2024-12-13 17:10:48 +0400

    New images

--- project/scripts/main.js
+++ project/scripts/main.js
@@ -63,17 +63,17 @@ async function loadNews() {
         {
             title: "Elden Ring: Nightreign",
             text: "We played the new Elden Ring spin-off and talked with FromSoftware about what to expect from the 2025 game.",
-            imgPath: "images/news/elden-ring.jpg",
+            imgPath: "images/news/elden-ring.webp",
         },
         {
             title: "WoW Classic Phase 1 - Molten Core",
             text: "Our Class Writers have prepared Best-in-Slot Gear Guides for every Class and Role combination for WoW Classic Phase 1. Between Molten Core and Onyxia's Lair, there are plenty of options to choose from - and we've looked at all of them!",
-            imgPath: "images/news/wow.jpg",
+            imgPath: "images/news/wow.webp",
         },
         {
             title: "Diablo 4 Slay Holiday Event",
             text: "Starting on December 17, 10 a.m.—January 2, 10 a.m. PST, fight with your fellow wanderers across the globe to beat back the incursions of greedy mongrels for a heap of rewards. Band together with the entire Diablo IV community to reach global goblin slaying milestones, and tantalizing rewards will be yours for the taking.",
-            imgPath: "images/news/diablo.jpg",
+            imgPath: "images/news/diablo.webp",
         },
     ];
 
